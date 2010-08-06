eb178d9ac9c9fb33a3aa89096b49e79f750db0e4
hachque <devnull@localhost>
2010-08-06 10:39:44 +0000

    Updated TODO list and added Environment class.

---
+++ appfs/applib/environment.cpp
@@ -0,0 +1,27 @@
+/*
+
+Code file for Environment.
+
+This class provides functions for interacting with the
+current environment, such as accessing environment variables
+and validating the existance of binaries.
+
+Last edited by: James Rhodes <jrhodes@roket-enterprises.com>,
+                6th August 2010
+
+This software is licensed under an MIT license.  See
+http://code.google.com/p/apptools-dist for more information.
+
+*/
+
+#include "config.h"
+#include <string>
+#include <vector>
+
+namespace AppLib
+{
+	std::vector<bool> Environment::searchForBinaries(std::vector<std::string> binary)
+	{
+	}
+}
+
